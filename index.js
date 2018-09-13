0be98ea8fbcd27a6ee5a8122ac5e301b1abf5626
Brian Yu <brianyu28@gmail.com>
2018-09-13 09:18:18 -0400

    Distribution code

---
+++ index.js
@@ -0,0 +1,30 @@
+const API_KEY = "API_KEY_HERE";
+
+window.onload = () => {
+  const url = `https://api.harvardartmuseums.org/gallery?apikey=${API_KEY}`;
+  showGalleries(url);
+}
+
+function showGalleries(url) {
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    data.records.forEach(gallery => {
+      document.querySelector("#galleries").innerHTML += `
+        <li>
+          <a href="#${gallery.id}" onclick="showObjectsTable(${gallery.id})">
+            Gallery #${gallery.id}: ${gallery.name} (Floor ${gallery.floor})
+          </a>
+        </li>
+      `;
+    });
+    if (data.info.next) {
+      showGalleries(data.info.next);
+    }
+  })
+}
+
+function showObjectsTable(id) {
+  document.querySelector("#all-objects").style.display = "block";
+  document.querySelector("#all-galleries").style.display = "none";
+}
